6c7a45a75849900d99e1b20433ae53a1d8cf5efb
ModaleSiemens <simonedemelas05@gmail.com>
2024-05-31 12:10:54 +0200

    Added Collection subproject; Renamed files

--- include/nets.hpp
+++ include/nets.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../subprojects/collection/include/collection.hpp"
+
 #include "types.hpp"
 #include "tcp_server.hpp"
 #include "tcp_client.hpp"
---
+++ src/tcp_client.cpp
@@ -0,0 +1,1 @@
+#include "../include/tcp_client.hpp"
