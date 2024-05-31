66e6a2860294d03437a95559d8d3c71123386e6e
ModaleSiemens <simonedemelas05@gmail.com>
2024-05-31 23:57:09 +0200

    Modified constructor prototype

--- include/tcp_remote.hpp
+++ include/tcp_remote.hpp
@@ -7,11 +7,15 @@ namespace nets
     class TcpRemote
     {
         public:
-            TcpRemote(nets::TcpSocket& socket);
+            //TcpRemote(nets::TcpSocket& socket);
+            TcpRemote(boost::asio::io_context& io_context);
 
             std::string getAddress() const;
             nets::Port  getPort()    const;
 
             nets::TcpSocket& getSocket();
+
+        private:
+            nets::TcpSocket socket;
     };
 }
